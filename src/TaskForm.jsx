be9814477adae582c55369f73f502e7053ceb52f
Andrea Fourel <andreafourel@hotmail.fr>
2023-03-16 14:43:03 +0100

    validation des champs

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -9,31 +9,50 @@ function TaskForm({ isTimerStarted, onSubmit }) {
   const initialFormValue = {
     title : '',
     description: '',
-  }
+  };
 
-  const [formValue, setFormValue] = useState(initialFormValue)
+  const [formValue, setFormValue] = useState(initialFormValue);
+  const [error, setError] = useState(null);
+
 
   const handleSubmitForm = (event) => {
     event.preventDefault();
+    if(error) {
+      alert(`${error.field}: ${error.message}`);
+      return;
+    }
     onSubmit(formValue);
     if(isTimerStarted) {
       setFormValue(initialFormValue);
     }
   }
 
+  const handleInputChange = (field, value) => {
+    if(field === 'title' && (!value || value.length < 4)){
+      setError({
+        field,
+        message: 'Title must have at least 4 sharacters',
+      });
+    } else {
+      setError(null);
+    }
+    setFormValue({...formValue, [field]: value})
+  }
+
   return (
     <form onSubmit={ handleSubmitForm } >
       <TextField 
-        placeholder='Titre de votre têche' 
+        placeholder='Titre de votre tâche' 
         labelTitle='Titre' 
         value={ formValue.title }
-        onChange={(v) => setFormValue({...formValue, title:v}) }
+        onChange={(v) => handleInputChange('title', v) }
       />
+      {error && error.field === 'title' && <p style={{color: 'red'}}>{error.message}</p>}
       <TextareaField 
         placeholder='Ecrivez votre description ici...' 
         labelTitle='Description' 
         value={ formValue.description }
-        onChange={(v) => setFormValue({...formValue, description:v}) }
+        onChange={(v) => handleInputChange('description', v) }
       />
       <Button type='submit' isTimerStarted={ isTimerStarted } />
     </form>
